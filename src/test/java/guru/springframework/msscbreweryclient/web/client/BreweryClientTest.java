95e9f5316f5f91ca5f184b8f78dab9bb58a7b0cb
Sadhana <sadhanasrinivasan00@gmail.com>
2021-10-13 16:10:10 +0530

    get done

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+
+@SpringBootTest
+class BreweryClientTest {
+	
+	@Autowired
+	BreweryClient client;
+
+	@Test
+	void testGetBeerById() {
+		BeerDto dto=client.getBeerById(UUID.randomUUID());
+		assertNotNull(dto);
+	}
+
+}
